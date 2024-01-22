62ff0400df5bbe436653df1f8e070220ccbd99e8
Володя <144096863+Dilllisha@users.noreply.github.com>
2024-01-22 19:06:26 +0300

    Update start.py

--- project/start.py
+++ project/start.py
@@ -1,10 +1,18 @@
 from pa.app import *
 from vol.vkbot import *
 import asyncio
+from multiprocessing import Process
 
-print('готово')
+def start():
+    uvicorn.run(app, host="0.0.0.0", port=80)
+    print("site online")
+if __name__ == '__main__':
+    p = Process(target=bot)
+    b = Process(target=start)
+    p.start()
+    b.start()
+    print("all loaded")
 
-uvicorn.run(app, host="0.0.0.0", port=80)
 
 
 
