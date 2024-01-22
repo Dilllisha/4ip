3e92b832f3da67193d2ba54e3530e4f9201d034d
Володя <144096863+Dilllisha@users.noreply.github.com>
2024-01-22 14:52:28 +0300

    Update start.py

--- project/start.py
+++ project/start.py
@@ -2,6 +2,11 @@ from pa.app import *
 from vol.vkbot import *
 
 print('готово')
-uvicorn.run(app, host="0.0.0.0", port=80)
-bot()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(asyncio.ensure_future(uvicorn.run(app, host="0.0.0.0", port=80)))
+loop.run_until_complete(asyncio.ensure_future(bot()))
+loop.close()
+
+
     
