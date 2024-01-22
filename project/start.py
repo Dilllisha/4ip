75622115306d136ef7a4f8bf71ea7109233643ca
Володя <144096863+Dilllisha@users.noreply.github.com>
2024-01-22 14:56:23 +0300

    Update start.py

--- project/start.py
+++ project/start.py
@@ -7,7 +7,7 @@ print('готово')
 loop = asyncio.get_event_loop()
 
 futures = [
-    asyncio.ensure_future(uvicorn.run(app, host="0.0.0.0", port=80),
+    asyncio.ensure_future(uvicorn.run(app, host="0.0.0.0", port=80)),
     asyncio.ensure_future(bot())
 ]
 
