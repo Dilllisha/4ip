34682ea43b53dcda48628ef06a30aed9bde70c47
Володя <144096863+Dilllisha@users.noreply.github.com>
2024-01-23 12:12:14 +0300

    Update bot.py

--- project/rom/bot.py
+++ project/rom/bot.py
@@ -7,12 +7,12 @@ from vk_api.utils import get_random_id
 
 def botik():
     token = "vk1.a.Jx9Hqyr4HIjGO5QHHrF0bIlTsiNFB2Bv5Jdxm9bNsOBa9dvIUCECnv2cA9SjGyFGhkR8dRojqrHIUjw8gUga8QtACMHNZ656r4HKK_b_hhufu17cUn20XumQ4zll2aetLgFxttiwWx-o1c4Oy-YHdGna1BTxdj9oDOIfAvSAmZqqQ9aIc_ux4rRtL77T2mFAqP0TAuz-aD0ICXRrfdiImQ"
-    image = "D:/python/Mihaylov/photo/menu.png"
+    # image = "D:/python/Mihaylov/photo/menu.png"
     
     
     session = vk_api.VkApi(token=token)
     longpoll = VkLongPoll(session)
-    upload = VkUpload(session)
+    # upload = VkUpload(session)
     now = datetime.now()
     current_time = now.strftime("%H:%M")
     welcome = ["хай", "привет", "ку", "yo"]
@@ -34,15 +34,15 @@ def botik():
         session.method("messages.send", post)
     
     
-    def sender_photo(id, text ):
-        post = {
-            "user_id": id,
-            "message": text,
-            "random_id": 0,
-            "attachment": ','.join(attachments)
+    # def sender_photo(id, text ):
+    #     post = {
+    #         "user_id": id,
+    #         "message": text,
+    #         "random_id": 0,
+    #         "attachment": ','.join(attachments)
     
-        }
-        session.method("messages.send", post)
+    #     }
+    #     session.method("messages.send", post)
     
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
@@ -50,10 +50,10 @@ def botik():
             user_id = event.user_id
     
             if text in welcome:
-                attachments = []
-                upload_image = upload.photo_messages(photos=image)[0]
-                attachments.append('photo{}_{}'.format(upload_image['owner_id'], upload_image['id']))
-                sender_photo(user_id, "хай")
+                # attachments = []
+                # upload_image = upload.photo_messages(photos=image)[0]
+                # attachments.append('photo{}_{}'.format(upload_image['owner_id'], upload_image['id']))
+                sender(user_id, "хай")
     
             elif text == "меню":
     
