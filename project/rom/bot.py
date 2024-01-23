581e446ec82dd057f9db446f25e31e366e5b7ba9
Володя <144096863+Dilllisha@users.noreply.github.com>
2024-01-23 12:38:35 +0300

    Update bot.py

--- project/rom/bot.py
+++ project/rom/bot.py
@@ -5,46 +5,46 @@ from vk_api import VkUpload
 from datetime import datetime
 from vk_api.utils import get_random_id
 
+
+print("Romik")
+token = "vk1.a.Jx9Hqyr4HIjGO5QHHrF0bIlTsiNFB2Bv5Jdxm9bNsOBa9dvIUCECnv2cA9SjGyFGhkR8dRojqrHIUjw8gUga8QtACMHNZ656r4HKK_b_hhufu17cUn20XumQ4zll2aetLgFxttiwWx-o1c4Oy-YHdGna1BTxdj9oDOIfAvSAmZqqQ9aIc_ux4rRtL77T2mFAqP0TAuz-aD0ICXRrfdiImQ"
+# image = "D:/python/Mihaylov/photo/menu.png"
+
+
+session = vk_api.VkApi(token=token)
+longpoll = VkLongPoll(session)
+# upload = VkUpload(session)
+now = datetime.now()
+current_time = now.strftime("%H:%M")
+welcome = ["хай", "привет", "ку", "yo"]
+print("бот запущен", current_time)
+
+def sender(id, text, keyboard=None):
+    post = {
+            "user_id": id,
+            "message": text,
+            "random_id": 0,
+
+    }
+
+    if keyboard != None:
+        post["keyboard"] = keyboard.get_keyboard()
+    else:
+        post = post
+
+    session.method("messages.send", post)
+
+
+# def sender_photo(id, text ):
+#     post = {
+#         "user_id": id,
+#         "message": text,
+#         "random_id": 0,
+#         "attachment": ','.join(attachments)
+
+#     }
+#     session.method("messages.send", post)
 def botik():
-    print("Romik")
-    token = "vk1.a.Jx9Hqyr4HIjGO5QHHrF0bIlTsiNFB2Bv5Jdxm9bNsOBa9dvIUCECnv2cA9SjGyFGhkR8dRojqrHIUjw8gUga8QtACMHNZ656r4HKK_b_hhufu17cUn20XumQ4zll2aetLgFxttiwWx-o1c4Oy-YHdGna1BTxdj9oDOIfAvSAmZqqQ9aIc_ux4rRtL77T2mFAqP0TAuz-aD0ICXRrfdiImQ"
-    # image = "D:/python/Mihaylov/photo/menu.png"
-    
-    
-    session = vk_api.VkApi(token=token)
-    longpoll = VkLongPoll(session)
-    # upload = VkUpload(session)
-    now = datetime.now()
-    current_time = now.strftime("%H:%M")
-    welcome = ["хай", "привет", "ку", "yo"]
-    print("бот запущен", current_time)
-    
-    def sender(id, text, keyboard=None):
-        post = {
-                "user_id": id,
-                "message": text,
-                "random_id": 0,
-    
-        }
-    
-        if keyboard != None:
-            post["keyboard"] = keyboard.get_keyboard()
-        else:
-            post = post
-    
-        session.method("messages.send", post)
-    
-    
-    # def sender_photo(id, text ):
-    #     post = {
-    #         "user_id": id,
-    #         "message": text,
-    #         "random_id": 0,
-    #         "attachment": ','.join(attachments)
-    
-    #     }
-    #     session.method("messages.send", post)
-    
     for event in longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
             text = event.text.lower()
