fec4adb58578fe3f848c29f72034f8a20bc9cff1
Володя <144096863+Dilllisha@users.noreply.github.com>
2024-01-23 12:05:52 +0300

    Update start.py

--- project/start.py
+++ project/start.py
@@ -1,16 +1,16 @@
-from pa.app import *
-from vol.vkbot import *
+# from pa.app import *
+# from vol.vkbot import *
 from rom.bot import *
 import asyncio
 from multiprocessing import Process
 
-def run():
-    uvicorn.run(app, host="0.0.0.0", port=80)
+# def run():
+#     uvicorn.run(app, host="0.0.0.0", port=80)
     
 if __name__ == "__main__":
-    Process(target=botik).start()
-    Process(target=bot).start()
-    Process(target=run).start()
+    # Process(target=botik).start()
+    # Process(target=bot).start()
+    # Process(target=run).start()
     
 
 
